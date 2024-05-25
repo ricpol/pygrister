2132ff65dd1589ab05f816e595586d22f757aaaa
ricpol <ric.pol@libero.it>
2024-05-25 19:04:22 +0200

    clean config

--- src/pygrister/config.py
+++ src/pygrister/config.py
@@ -2,12 +2,12 @@
 # Use only non-empty strings as config values.
 
 PYGRISTER_CONFIG = {
-    'GRIST_API_KEY': 'e6860d9d937f76a1a9a1e527ec0eceb1baf4c82b',
+    'GRIST_API_KEY': '<your_api_key_here>',
     'GRIST_SERVER_PROTOCOL': 'https://',
-    'GRIST_TEAM_SITE': 'apitestteam',
+    'GRIST_TEAM_SITE': 'docs',
     'GRIST_API_SERVER': 'getgrist.com/api',
-    'GRIST_WORKSPACE_ID': '64302',
-    'GRIST_DOC_ID': 'dcQeRc9ShPDttdWkFc71SL',
+    'GRIST_WORKSPACE_ID': '<your_ws_id_here>',
+    'GRIST_DOC_ID': '<your_doc_id_here>',
     'GRIST_RAISE_ERROR': 'Y',
     'GRIST_SAFEMODE': 'N',
 }
