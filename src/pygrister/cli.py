39a3e140e92ea9d7f17d4d96ec3482b2d69461a0
ricpol <ric.pol@libero.it>
2025-05-10 15:08:23 +0200

    added inspect option

--- src/pygrister/cli.py
+++ src/pygrister/cli.py
@@ -56,10 +56,10 @@ class _CliConfigurator(Configurator):
         # updating config at runtime is not supported
         raise NotImplementedError
 
-def _get_pygrist() -> GristApi:
-    c = _CliConfigurator()
-    g = GristApi(custom_configurator=c)
-    return g
+# the global GristApi (re-creadted at every cli call): inside a cli function, 
+# all api calls (usually just one but may be more) will use this instance
+_c = _CliConfigurator()
+the_grist = GristApi(custom_configurator=_c)
 
 BADCALL = 3 # the exit code we reserve for bad call errors (eg http 404)
 DONEMSG = '[green]Done.[/green]'
@@ -67,17 +67,23 @@ ERRMSG = '[bold red]Error![/bold red]'
 
 # a few helper functions
 # ----------------------------------------------------------------------
-def _exit_with_error(st, res) -> None:
+def _exit_with_error(st, res, inspect) -> None:
+    if inspect:
+        print(the_grist.inspect())
     print(ERRMSG, 'Status:', st, res)
     raise typer.Exit(BADCALL)
 
-def _exit_if_error(st, res, ok) -> None:
-    if not ok:
+def _exit_if_error(st, res, inspect) -> None:
+    if not the_grist.ok:
+        if inspect:
+            print(the_grist.inspect())
         print(ERRMSG, 'Status:', st, res)
         raise typer.Exit(BADCALL)
 
-def _print_done_or_exit(st, res, ok) -> None:
-    if ok: 
+def _print_done_or_exit(st, res, inspect) -> None:
+    if inspect:
+        print(the_grist.inspect())
+    if the_grist.ok: 
         print(DONEMSG)
     else:
         print(ERRMSG, 'Status:', st, res)
@@ -103,6 +109,8 @@ def _user_access_validate(value):
 # ----------------------------------------------------------------------
 details_opt = typer.Option('--details/--no-details', '-D/-d',
                            help='Long/short info')
+inspect_opt = typer.Option('--inspect', '-i', 
+                           help = 'Print inspect output after api call')
 team_id_opt = typer.Option('--team', '-t', 
                            help='The team ID [default: current]')
 ws_id_opt = typer.Option('--ws', '-w', 
@@ -123,24 +131,27 @@ app.add_typer(ws_app, name='ws', no_args_is_help=True)
 # gry team -> for managing team sites (organisations)
 # ----------------------------------------------------------------------
 @org_app.command('list')
-def list_orgs() -> None:
+def list_orgs(inspect: Annotated[bool, inspect_opt] = False) -> None:
     """List the teams you have access to"""
-    g = _get_pygrist()
-    st, res = g.list_team_sites()
-    _exit_if_error(st, res, g.ok)
+    st, res = the_grist.list_team_sites()
+    _exit_if_error(st, res, inspect)
     console = Console()
     table = Table('id', 'name', 'owner')
     for org in res:
         table.add_row(str(org['id']), org['name'], org['owner']['name'])
+    if inspect:
+        print(the_grist.inspect())
     console.print(table)
 
 @org_app.command('see')
 def see_org(team_id: Annotated[str, team_id_opt] = '', 
-            details: Annotated[bool, details_opt] = False) -> None:
+            details: Annotated[bool, details_opt] = False,
+            inspect: Annotated[bool, inspect_opt] = False) -> None:
     """Describe a team"""
-    g = _get_pygrist()
-    st, res = g.see_team(team_id)
-    _exit_if_error(st, res, g.ok) 
+    st, res = the_grist.see_team(team_id)
+    _exit_if_error(st, res, inspect) 
+    if inspect:
+        print(the_grist.inspect())
     if details:
         print(res)
     else:
@@ -154,26 +165,28 @@ def see_org(team_id: Annotated[str, team_id_opt] = '',
 
 @org_app.command('update')
 def update_org(name: Annotated[str, typer.Argument(help='The new name')], 
-               team_id: Annotated[str, team_id_opt] = '') -> None:
+               team_id: Annotated[str, team_id_opt] = '',
+               inspect: Annotated[bool, inspect_opt] = False) -> None:
     """Change the team name"""
-    g = _get_pygrist()
-    st, res = g.update_team(name, team_id)
-    _print_done_or_exit(st, res, g.ok)
+    st, res = the_grist.update_team(name, team_id)
+    _print_done_or_exit(st, res, inspect)
      
 @org_app.command('delete')
-def delete_org(team_id: Annotated[str, team_id_opt] = '') -> None:
+def delete_org(team_id: Annotated[str, team_id_opt] = '',
+               inspect: Annotated[bool, inspect_opt] = False) -> None:
     """Delete a team"""
-    g = _get_pygrist()
-    st, res = g.delete_team(team_id)
-    _print_done_or_exit(st, res, g.ok)
+    st, res = the_grist.delete_team(team_id)
+    _print_done_or_exit(st, res, inspect)
 
 @org_app.command('users')
 def list_org_users(team_id: Annotated[str, team_id_opt] = '',  
-                   details: Annotated[bool, details_opt] = False) -> None:
+                   details: Annotated[bool, details_opt] = False,
+                   inspect: Annotated[bool, inspect_opt] = False) -> None:
     """List users with access to team"""
-    g = _get_pygrist()
-    st, res = g.list_team_users(team_id)
-    _exit_if_error(st, res, g.ok)
+    st, res = the_grist.list_team_users(team_id)
+    _exit_if_error(st, res, inspect)
+    if inspect:
+        print(the_grist.inspect())
     if details:
         print(res)
     else:
@@ -182,21 +195,23 @@ def list_org_users(team_id: Annotated[str, team_id_opt] = '',
 @org_app.command('user-access')
 def change_team_access(uid: Annotated[int, typer.Argument(help='The user ID')], 
                        access: Annotated[str, access_opt],
-                       team_id: Annotated[str, team_id_opt] = '') -> None:
+                       team_id: Annotated[str, team_id_opt] = '',
+                       inspect: Annotated[bool, inspect_opt] = False) -> None:
     """Change user access level to a team"""
-    g = _get_pygrist()
-    st, res = g.list_team_users(team_id)
-    _exit_if_error(st, res, g.ok)
+    st, res = the_grist.list_team_users(team_id)
+    _exit_if_error(st, res, inspect)
     usr_email = ''
     for usr in res:
         if usr['id'] == uid:
             usr_email = usr['email']
             break
     if not usr_email:
+        if inspect:
+            print(the_grist.inspect())
         raise typer.BadParameter('User id not found.')
     users = {usr_email: access}
-    st, res = g.update_team_users(users, team_id)
-    _print_done_or_exit(st, res, g.ok)
+    st, res = the_grist.update_team_users(users, team_id)
+    _print_done_or_exit(st, res, inspect)
         
 #TODO "change_user_access" is only for existing users, but "update_team_users"
 # allows for adding users too. Maybe add a separate cli endpoint for this?
@@ -205,11 +220,13 @@ def change_team_access(uid: Annotated[int, typer.Argument(help='The user ID')],
 # ----------------------------------------------------------------------
 @ws_app.command('list')
 def list_ws(team_id: Annotated[str, team_id_opt] = '', 
-            details: Annotated[bool, details_opt] = False) -> None:
+            details: Annotated[bool, details_opt] = False,
+            inspect: Annotated[bool, inspect_opt] = False) -> None:
     """List workspaces and documents in a team"""
-    g = _get_pygrist()
-    st, res = g.list_workspaces(team_id)
-    _exit_if_error(st, res, g.ok)
+    st, res = the_grist.list_workspaces(team_id)
+    _exit_if_error(st, res, inspect)
+    if inspect:
+        print(the_grist.inspect())
     if details:
         print(res)
     else:
@@ -223,20 +240,24 @@ def list_ws(team_id: Annotated[str, team_id_opt] = '',
 
 @ws_app.command('new')
 def add_ws(name: Annotated[str, typer.Argument(help='The name of new ws')], 
-           team_id: Annotated[str, team_id_opt] = '') -> None:
+           team_id: Annotated[str, team_id_opt] = '',
+           inspect: Annotated[bool, inspect_opt] = False) -> None:
     """Create an empty workspace"""
-    g = _get_pygrist()
-    st, res = g.add_workspace(name, team_id)
-    _exit_if_error(st, res, g.ok)
+    st, res = the_grist.add_workspace(name, team_id)
+    _exit_if_error(st, res, inspect)
+    if inspect:
+        print(the_grist.inspect())
     print(DONEMSG, 'Id:', res)
 
 @ws_app.command('see')
 def see_ws(ws_id: Annotated[int, ws_id_opt] = 0,
-           details: Annotated[bool, details_opt] = False) -> None:
+           details: Annotated[bool, details_opt] = False,
+           inspect: Annotated[bool, inspect_opt] = False) -> None:
     """Describe a workspace"""
-    g = _get_pygrist()
-    st, res = g.see_workspace(ws_id)
-    _exit_if_error(st, res, g.ok)
+    st, res = the_grist.see_workspace(ws_id)
+    _exit_if_error(st, res, inspect)
+    if inspect:
+        print(the_grist.inspect())
     if details:
         print(res)
     else:
@@ -252,26 +273,28 @@ def see_ws(ws_id: Annotated[int, ws_id_opt] = 0,
 
 @ws_app.command('update')
 def update_ws(name: Annotated[str, typer.Argument(help='The new name')], 
-              ws_id: Annotated[int, ws_id_opt] = 0) -> None:
+              ws_id: Annotated[int, ws_id_opt] = 0,
+              inspect: Annotated[bool, inspect_opt] = False) -> None:
     """Change the workspace name"""
-    g = _get_pygrist()
-    st, res = g.update_workspace(name, ws_id)
-    _print_done_or_exit(st, res, g.ok)
+    st, res = the_grist.update_workspace(name, ws_id)
+    _print_done_or_exit(st, res, inspect)
 
 @ws_app.command('delete')
-def delete_ws(ws_id: Annotated[int, ws_id_opt] = 0) -> None:
+def delete_ws(ws_id: Annotated[int, ws_id_opt] = 0,
+              inspect: Annotated[bool, inspect_opt] = False) -> None:
     """Delete a workspace"""
-    g = _get_pygrist()
-    st, res = g.delete_workspace(ws_id)
-    _print_done_or_exit(st, res, g.ok)
+    st, res = the_grist.delete_workspace(ws_id)
+    _print_done_or_exit(st, res, inspect)
 
 @ws_app.command('users')
 def list_ws_users(ws_id: Annotated[int, ws_id_opt] = 0,  
-                  details: Annotated[bool, details_opt] = False) -> None:
+                  details: Annotated[bool, details_opt] = False,
+                  inspect: Annotated[bool, inspect_opt] = False) -> None:
     """List users with access to workspace"""
-    g = _get_pygrist()
-    st, res = g.list_workspace_users(ws_id)
-    _exit_if_error(st, res, g.ok)
+    st, res = the_grist.list_workspace_users(ws_id)
+    _exit_if_error(st, res, inspect)
+    if inspect:
+        print(the_grist.inspect())
     if details:
         print(res)
     else:
@@ -280,21 +303,23 @@ def list_ws_users(ws_id: Annotated[int, ws_id_opt] = 0,
 @ws_app.command('user-access')
 def change_ws_access(uid: Annotated[int, typer.Argument(help='The user ID')], 
                      access: Annotated[str, access_opt],
-                     ws_id: Annotated[int, ws_id_opt] = 0) -> None:
+                     ws_id: Annotated[int, ws_id_opt] = 0,
+                     inspect: Annotated[bool, inspect_opt] = False) -> None:
     """Change user access level to a workspace"""
-    g = _get_pygrist()
-    st, res = g.list_workspace_users(ws_id)
-    _exit_if_error(st, res, g.ok)
+    st, res = the_grist.list_workspace_users(ws_id)
+    _exit_if_error(st, res, inspect)
     usr_email = ''
     for usr in res:
         if usr['id'] == uid:
             usr_email = usr['email']
             break
     if not usr_email:
+        if inspect:
+            print(the_grist.inspect())
         raise typer.BadParameter('User id not found.')
     users = {usr_email: access}
-    st, res = g.update_workspace_users(users, ws_id)
-    _print_done_or_exit(st, res, g.ok)
+    st, res = the_grist.update_workspace_users(users, ws_id)
+    _print_done_or_exit(st, res, inspect)
         
 #TODO "change_user_access" is only for existing users, but "update_team_users"
 # allows for adding users too. Maybe add a separate cli endpoint for this?
