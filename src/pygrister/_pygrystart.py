95b4a858df1288cabd8ebedff6a87e11115901f7
ricpol <ric.pol@libero.it>
2025-11-14 15:45:17 +0100

    pass Requests options to gry python

--- src/pygrister/_pygrystart.py
+++ src/pygrister/_pygrystart.py
@@ -3,10 +3,12 @@
 from sysconfig import get_python_version, get_platform
 from pygrister import __version__
 from pygrister.api import GristApi
-from pygrister.cli import _CliConfigurator, cli_in_converters, cli_out_converters # type: ignore
+from pygrister.cli import (
+    _CliConfigurator, cli_in_converters, cli_out_converters, req_options) # type: ignore
 gry = GristApi(custom_configurator=_CliConfigurator())
 gry.in_converter = cli_in_converters
 gry.out_converter = cli_out_converters
+gry.apicaller.request_options = req_options
 print(f'This is Python {get_python_version()} on {get_platform()}, and Pygrister {__version__}.')
 print('Here, "gry" is a ready-to-use, pre-configured GristApi instance.')
 del get_python_version
@@ -14,4 +16,5 @@ del get_platform
 del _CliConfigurator
 del cli_in_converters
 del cli_out_converters
+del req_options
 del __version__
